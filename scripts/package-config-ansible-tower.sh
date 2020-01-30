adadc6d08b6a5e775a2435d8a438db8a44fd92f2
tosin Akinosho <takinosh@redhat.com>
2020-01-30 10:00:06 -0500

    adding new version of ansible tower config

--- scripts/package-config-ansible-tower.sh
+++ scripts/package-config-ansible-tower.sh
@@ -2,15 +2,15 @@
 # Ansible tower cli quick install
 if [[ -z $1 ]]; then
   echo "USAGE: $0 poolid"
+  echo "To list poolids: sudo subscription-manager list --available"
   exit 1
 fi
 POOL_ID=$1
 
-sudo subscription-manager register
 sudo subscription-manager attach --pool=$POOL_ID
 #subscription-manager attach --auto
 
-sudo subscription-manager repos --disable="*" 
+sudo subscription-manager repos --disable="*"
 sudo subscription-manager repos --enable  rhel-7-server-ansible-2.6-rpms
 sudo subscription-manager repos --enable rhel-server-rhscl-7-rpms
 sudo subscription-manager repos \
@@ -19,6 +19,8 @@ sudo subscription-manager repos \
    --enable="rhel-7-server-ose-3.11-rpms"
 
 sudo yum install ansible vim -y
+#https://developers.redhat.com/blog/2018/08/13/install-python3-rhel/
+#^^ansible python3 pip
 sudo yum install python27-python-pip -y
 sudo yum install openshift-ansible -y
 
